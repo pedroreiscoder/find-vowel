556b9be14a32a0af3fad8e14ec0744809a3fcd78
pedroreis <pedroreiscoder@gmail.com>
2021-01-10 20:42:12 -0300

    Testes unitarios finalizados

---
+++ FindVowel/FindVowel.Tests/FindVowelTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+
+namespace FindVowel.Tests
+{
+    [TestClass]
+    public class FindVowelTest
+    {
+        [TestMethod]
+        public void DeveriaRetornarFirstChar1()
+        {
+            string data = "aAbBABacfe";
+            FindVowelReader findVowelReader = new FindVowelReader(new MemoryStream(Encoding.UTF8.GetBytes(data)));
+
+            char? firstChar = FindVowel.FirstChar(findVowelReader);
+
+            Assert.AreEqual('e', firstChar);
+        }
+
+        [TestMethod]
+        public void DeveriaRetornarFirstChar2()
+        {
+            string data = "eBduFGtabGUe";
+            FindVowelReader findVowelReader = new FindVowelReader(new MemoryStream(Encoding.UTF8.GetBytes(data)));
+
+            char? firstChar = FindVowel.FirstChar(findVowelReader);
+
+            Assert.AreEqual('a', firstChar);
+        }
+
+        [TestMethod]
+        public void DeveriaRetornarNulo()
+        {
+            string data = "bCaEjKiaLmi";
+            FindVowelReader findVowelReader = new FindVowelReader(new MemoryStream(Encoding.UTF8.GetBytes(data)));
+
+            char? firstChar = FindVowel.FirstChar(findVowelReader);
+
+            Assert.IsNull(firstChar);
+        }
+    }
+}
